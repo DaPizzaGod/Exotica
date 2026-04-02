d37cdc7c82566e18384d2cdb6db740473bf7610d
Da_Pizza <advaithyt123@gmail.com>
2026-04-02 00:28:55 -0400

    finished idle state

--- entities/prototype/player/idle.gd
+++ entities/prototype/player/idle.gd
@@ -1,1 +1,21 @@
 extends State
+
+# other states
+
+func enter() -> void:
+	parent.velocity.x = 0
+
+func check_input(_event: InputEvent) -> State:
+	if Input.is_action_just_pressed("jump") and parent.is_on_floor():
+		return #jump
+	if Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
+		return #move
+	return null
+
+func check_physics(delta: float) -> State:
+	parent.velocity.y += gravity * delta
+	parent.move_and_slide()
+	
+	if !parent.is_on_floor():
+		return #fall
+	return null
