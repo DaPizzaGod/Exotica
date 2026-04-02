78762f3b13096eb8732a22e765c8f9e4e38ebe0e
Da_Pizza <advaithyt123@gmail.com>
2026-04-02 15:20:16 -0400

    before momentum

--- entities/prototype/player/player.gd
+++ entities/prototype/player/player.gd
@@ -2,6 +2,12 @@ extends CharacterBody2D
 
 @onready var state_machine:= $StateMachine
 @onready var sprite: Sprite2D = $Sprite2D
+var momentum_x: float
+var momentum_y: float
 
 func _ready() -> void:
 	state_machine.start_state_machine(self)
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(momentum_x, momentum_y)
+	move_and_slide()
